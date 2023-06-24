3b56cb4efedf60d473bfcdd85ad7af3d4827b1f0
Laurence Warne <laurencewarne@gmail.com>
2023-06-24 16:06:45 +0100

    Make polybar apply to all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,14 +8,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+# Launch top and bottom config on all monitors
 
-if [[ $1 == "--timeless" ]];
-then
-  echo "doing timeless"
-  polybar top -c ~/.config/polybar/config-top-timeless.ini &
-else
-  polybar top -c ~/.config/polybar/config-top.ini &
-fi
-
-polybar bottom -c ~/.config/polybar/config-bottom.ini &
+# See https://github.com/polybar/polybar/issues/763
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  if [[ $1 == "--timeless" ]];
+  then
+    echo "doing timeless"
+    MONITOR=$m polybar top -c ~/.config/polybar/config-top-timeless.ini &
+  else
+    MONITOR=$m polybar top -c ~/.config/polybar/config-top.ini &
+  fi
+  
+  MONITOR=$m polybar bottom -c ~/.config/polybar/config-bottom.ini &
+done
