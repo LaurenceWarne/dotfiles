dee81882474cc346dc46cc58ebf19b301b38bdb4
Laurence Warne <laurencewarne@gmail.com>
2024-09-28 11:22:30 +0100

    Add material icons

--- .config/waybar/amdgpu.py
+++ .config/waybar/amdgpu.py
@@ -4,5 +4,5 @@ attrib = sys.argv[1]
 
 for l in sys.stdin:
     result = next(((c.split(" ")[-1][:-2]) for c in l.split(", ") if c.startswith(attrib + " ")), None)
-    if result and attrib == "vram": print(f"{float(result)/1024:.2g} G")
-    if result and attrib == "gpu": print(f"{result}")
+    if result and attrib == "vram": print(f"{float(result)/1024:.2g}G", flush=True)
+    if result and attrib == "gpu": print(f"{result}", flush=True)
